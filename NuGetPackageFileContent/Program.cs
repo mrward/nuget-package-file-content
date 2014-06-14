8b4f29899eae0a450b90d2c11b826215969f40fa
Matt Ward <ward.matt@gmail.com>
2014-06-14 20:54:54 +0100

    Initial revision.

---
+++ NuGetPackageFileContent/Program.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using System.IO;
+using System.Linq;
+
+using NuGet;
+
+namespace NuGetPackageFileContent
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			try {
+				var program = new Program();
+				program.Run();
+			} catch (Exception ex) {
+				Console.WriteLine(ex);
+			}
+		}
+		
+		void Run()
+		{
+			string url = "http://nuget.org/api/v2/";
+			var repository = PackageRepositoryFactory.Default.CreateRepository(url);
+			foreach (IPackage package in repository
+				.GetPackages()
+				.Where(p => p.IsAbsoluteLatestVersion)
+				.OrderByDescending(p => p.DownloadCount)
+				//.Skip(100)
+				.Take(100)
+				.AsCollapsed()) {
+				
+				foreach (IPackageFile file in package.GetContentFiles()) {
+					string extension = Path.GetExtension(file.Path);
+					if (".xdt" == extension.ToLower()) {
+						Console.WriteLine("Package.Id: " + package.Id);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
